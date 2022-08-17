3b8bc25c786c1b9dcc73d115b4c6f08868ceddbb
Unique-Red <vnoah410@gmail.com>
2022-08-17 05:17:27 -0700

    fix: pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 Island Perimeter
 """
 
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid
 
@@ -17,16 +18,16 @@ def island_perimeter(grid):
     """
     y = 0
     x = 0
-    
+
     for row in range(1, len(grid) - 1):
-            for col in range(1, len(grid[row]) - 1):
-                if grid[row][col] == 1:
-                    if grid[row][col - 1] == 0:
-                        y += 1
-                    if grid[row][col + 1] == 0:
-                        y += 1
-                    if grid[row - 1][col] == 0:
-                        x += 1
-                    if grid[row + 1][col] == 0:
-                        x += 1
+        for col in range(1, len(grid[row]) - 1):
+            if grid[row][col] == 1:
+                if grid[row][col - 1] == 0:
+                    y += 1
+                if grid[row][col + 1] == 0:
+                    y += 1
+                if grid[row - 1][col] == 0:
+                    x += 1
+                if grid[row + 1][col] == 0:
+                    x += 1
     return x + y
